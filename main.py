fd9afdf5d63210b233dda194ade8a7a801b731e9
Agustina Peña <aepenai@uc.cl>
2022-10-12 23:38:03 -0300

    E2 Semi terminado

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ A = ["Aceite de maravilla", "Arroz", "Avena", "Azúcar", "Crema de Leche", "Espi
 N = ["Magnesio", "Calcio", "Fosforo", "Sodio", "Potasio", "Hierro", "Zinc", "Yodo"]
 I = ["Santa Rosa", "San Diego", "Sierra Bella","Club Hípico", "Fantasilandia", "Bulnes","Metro Los Orientales", "Las Dalias", "Estadio Manquehue","Macul"]
 K = ["Santiago", "Recoleta", "Estación Central","Ñuñoa", "Macul", "Peñalolén","La Florida", "La Pintana", "El bosque","Providencia","San Bernardo","La Granja","San Miguel", "Cerrillos","Independencia"]
-B = ["1", "2", "3","4", "5"]
+B = ["1", "2", "3","4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "15", "16", "17"]
 #E = ["1", "2", "3","4", "5", "6","7", "8", "9","10", "11", "12","13", "14", "15"]
 #T = ["1", "2", "3","4", "5"]
 F = ["1", "2", "3","4", "5", "6","7", "8", "9","10", "11", "12","13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28","29", "30", "31","32", "33", "34","35", "36", "37","38", "39", "40", "41", "42", "43", "44", "45"]
@@ -30,7 +30,7 @@ F = ["1", "2", "3","4", "5", "6","7", "8", "9","10", "11", "12","13", "14", "15"
 
 
 ##  D_ik LISTO  ##
-C_max = 7000
+C_max = 700000
 H = 25000
 U = 220.45
 ##  F_i LISTO  ##
@@ -41,6 +41,8 @@ E_t = 300
 ##   M_a LISTO  ## 
 L = 1200
 Pre = 85000000000
+Big_M = 10^10000
+
 ##  N_ma LISTO  ## 
 ##  P_am LISTO  ##
 ##  C_mi LISTO  ##
@@ -52,7 +54,7 @@ Pre = 85000000000
 
 # Agregar Variables #
  
-# Cantidad de cajas “j” asignadas en un lugar de acopio “k”.#
+# Cantidad de cajas “j” asignadas en un lugar de acopio “k”#
 x_jk = modelo.addVars(J, K, vtype=GRB.INTEGER, name="x_jk")
 
 #Cantidad de alimento “a” enviado a “i” que es comprado a proveedor “m”.#
@@ -64,7 +66,7 @@ z_i = modelo.addVars(I, vtype=GRB.BINARY, name="z_i")
 # 1 si se compra al proveedor “m” y hace envío a bodega “i” # 
 v_mi = modelo.addVars(M, I, vtype=GRB.BINARY, name="v_mi")
 
-# 1 si el camión “b” tiene asignado un lugar de acopio “k” #
+# 1 si el camión “b” tiene asignado un lugar de acopio “k” desde la bodega i#
 phi_bki = modelo.addVars(B, K, I, vtype=GRB.BINARY, name="phi_bk")
 
 #cantidad de unidades de alimento “a” en la caja de tipo “j”
@@ -73,98 +75,97 @@ phi_bki = modelo.addVars(B, K, I, vtype=GRB.BINARY, name="phi_bk")
 # 1 si familia “i” vive cercano a centro de entrega “k” y se le entrega una caja tipo “j” #
 u_fjk = modelo.addVars(F, J, K, vtype=GRB.BINARY, name="u_fjk")
 
-
-
-
-
-# VARIABLES EN REVISIÓN #
-#1 si el camión “b” está siendo utilizado por un transportista “t”# EN REVISION
-#w_bt = modelo.addVars(B, T, vtype=GRB.BINARY, name="w_bt")
-
-# 1 si el trabajador “e” ya está en una bodega “i” #
-#g_ei = modelo.addVars(E, I, vtype=GRB.BINARY, name="g_ei")
-
-#1 si la caja de tipo “j” está asociada a un camión “b”#
-#lambda_jb = modelo.addVars(J, B, vtype=GRB.BINARY, name="lambda_jb")
-
-# Si la caja de tipo “j” se encuentra en la bodega “i” y 0 e.c.o.c #
-#y_ji = modelo.addVars(E, I, vtype=GRB.BINARY, name="y_ji")
-
-
 modelo.update()
 
 
 # Agregar Restricciones #
 
+
 modelo.addConstrs((quicksum(u_fjk[f,j,k] for f in F) == x_jk[j, k] for j in J for k in K), name = "R1")
 
-# modelo.addConstrs((u_fjk[f,j,k] >= u_fjk[f+1,j,k] for f in range(1,len(F)-1) for j in J for k in K), name = "R2")
+modelo.addConstrs((int(Q_fjk(f,j,k)) >= u_fjk[f,j,k] for f in F for j in J for k in K), name = "R2")
 
-#modelo.addConstrs((int(Q_fjk(f,j,k)) >= u_fjk[f,j,k] for f in F for j in J for k in K), name = "R3")
+modelo.addConstrs(((quicksum(phi_bki[b, k, i] for k in K for i in I)) <= 1 for b in B), name = "R3")
 
-# Restricción 4 implicita #
+modelo.addConstrs(((quicksum(float(F_i(i)) * z_i[i] for i in I) + quicksum(phi_bki[b,k,i] * (H + float(D_ik(k, i)) * (L + U)) for b in B for k in K for i in I) + quicksum(x_jk[j, k] for j in J for k in K) * E_t + quicksum(y_aim[a,i,m] for a in A for i in I for m in M) * int(P_am(a, m)) + quicksum(int(C_mi(m,i))*v_mi[m,i] for m in M for i in I)) <= Pre for a in A for m in M), name = "R4")
 
-for j in J:
-     for n in N:
-          #modelo.addConstrs((quicksum(A_an(a, n) * o_aj[a, int(j)] for a in A) <= R_nj(j, n)), name = "R5")
-          #modelo.addConstr(((quicksum(int(A_an(a, n)) * int(O_aj(a, j)) for a in A)) >= int(R_nj(j, n))), name = "R5")
-          pass
-# Restricción 8 #
+modelo.addConstrs((Big_M * z_i[i] >= quicksum(y_aim[a,i,m] for a in A for m in M) for i in I), name = "R5")
 
-#modelo.addConstrs(((quicksum(phi_bki[b, k, i] for k in K for i in I)) <= 1 for b in B), name = "R8")
+modelo.addConstrs((z_i[i] * len(M) >= quicksum(v_mi[m,i] for m in M) for i in I), name = "R6")
 
-# Restricción 9 #
+modelo.addConstrs(((quicksum(y_aim[a, i, m] for a in A)) >= z_i[i] for m in M for i in I), name = "R7")
 
-#modelo.addConstrs(((quicksum(phi_bki[b, k, i] for b in B)) * C_max <= quicksum(x_jk[j ,k] * float(M_a(a)) * int(O_aj(a,j)) for j in J for a in A) for k in K for i in I), name = "R9")
+modelo.addConstrs(((quicksum(y_aim[a, i, m] for a in A)) <= v_mi[m, i] * Big_M for m in M for i in I), name = "8")
 
-# Restricción 10 #
+modelo.addConstrs(((quicksum(phi_bki[b, k, i] for k in K for b in B)) <= z_i[i] * Big_M for i in I), name = "R9")
 
-for k in K:
-     for i in I:
-          for b in B:
-               if D_ik(k,i) != minimo(k):
-                    #modelo.addConstr((float(D_ik(k, i) -(minimo(k)))>=(1-(phi_bki[b,k,i])) ), name = "R10")
-                    pass
 
-# Restricción 11 #
+modelo.addConstrs((quicksum(phi_bki[b,k,i] for b in B for k in K) >= z_i[i] for i in I), name = "R10")
 
-#modelo.addConstrs(((quicksum(float(F_i(i)) for i in I) * z_i[i] + quicksum(phi_bki[b,k,i] for b in B for k in K for i in I) * (H + float(D_ik(k, i)) * (L + U)) + quicksum(x_jk[j, k] for j in J for k in K) * E_t + quicksum(y_aim[a,i,m] for a in A for i in I for m in M) * int(P_am(a, m)) + quicksum(int(C_mi(m,i))*v_mi[m,i] for m in M for i in I)) <= Pre for a in A for m in M), name = "R11")
 
-# Restricción 12 #
+modelo.addConstrs(((quicksum(y_aim[a, i, m] for i in I for m in M)) == quicksum(x_jk[j ,k] for j in J for k in K) * quicksum(int(O_aj(a, j)) for j in J) for a in A), name = "R11")
 
-Big_M = 10^10
-#modelo.addConstrs((Big_M * z_i[i] >= quicksum(y_aim[a,i,m] for a in A for m in M) for i in I), name = "R12.1")
-#modelo.addConstrs((z_i[i] * 4 >= quicksum(v_mi[m,i] for m in M) for i in I), name = "R12.2")
-#modelo.addConstrs(((quicksum(y_aim[a, i, m] for a in A)) <= v_mi[m,i] for m in M for i in I), name = "R12.3")
+modelo.addConstrs((quicksum(y_aim[a,i,m] for i in I) <= int(N_ma(a, m)) for m in M for a in A), name = "R12")
 
-# Restricción 13 #
-
-#modelo.addConstrs((quicksum(phi_bki[b,k,i] for b in B for k in K) >= z_i[i] for i in I), name = "R13")
-
-# Restricción 14 #
-
-#modelo.addConstr((quicksum(y_aim[a, i, m] for a in A for i in I for m in M)) == quicksum(x_jk[j ,k] for j in J for k in K) * quicksum(int(O_aj(a, j)) for a in A for j in J), name = "R14")
-
-#modelo.addConstrs((quicksum(y_aim[a,i,m] for i in I) <= int(N_ma(a, m)) for m in M for a in A), name = "R15")
-
-#modelo.addConstrs((quicksum(x_jk[j, k] for j in J) <= H_k(k) for k in K), name = "R16")
+modelo.addConstrs((quicksum(x_jk[j,k] for j in J)<= quicksum(phi_bki[b, k, i] * Big_M for b in B for i in I)for k in K), name = "R13")
 
 
 modelo.update()
 
-
 funcion_objetivo = quicksum(x_jk[j, k] for j in J for k in K)
+
 modelo.setObjective(funcion_objetivo, GRB.MAXIMIZE)
+
 modelo.optimize()
 
-for constr in modelo.getConstrs():
-    if constr.getAttr("slack") == 0:
-     print("----------------------------------------")
-     print(f"La restricción {constr} está activa")
-     print("----------------------------------------")
 
-modelo.printAttr("X")
+for constr in modelo.getConstrs():
+     if constr.getAttr("slack") == 0:
+          print("----------------------------------------")
+          print(f"La restricción {constr} está activa")
+          print("----------------------------------------")
+
 
 for j in J:
      for k in K:
           print(f"hay {x_jk[j,k].x} cajas de tipo {j} en la municipalidad {k}")
+
+
+for b in B:
+     for k in K:
+          for i in I:
+               var = phi_bki[b,k,i].x
+               if var == 1:
+                   print(f"El camión {b} esta asignado a {k} desde la bodega {i}")
+               if var == 0:
+                    pass
+for a in A:
+     for m in M:
+          for i in I:
+               var = y_aim[a,i,m].x
+               if var != 0:
+                    print(f"se le compra {var} de {a} al provedor {m} y se almacena en {i}")
+
+for i in I:
+     var = z_i[i].x
+     if var == 1:
+          print(f"La bodega {i} se esta usando")
+
+for m in M:
+     for i in I:
+          var = v_mi[m,i].x
+          if var == 1:
+               print(f"Se le compra a {m} y se guarda en {i}")
+          
+
+
+
+# RESTRICCIONES EN REVISIÓN #
+#modelo.addConstrs((u_fjk[str(f),j,k] >= u_fjk[str(f+1),j,k] for f in range(1,len(F)-1) for j in J for k in K), name = "R2")
+#modelo.addConstrs((quicksum(x_jk[j, k] for j in J) <= H_k(k) for k in K), name = "R16")
+
+#Se necesitará cuando se agrande la matriz
+#modelo.addConstrs(((quicksum(phi_bki[b, k, i] for b in B)) * C_max >= quicksum(x_jk[j ,k] * float(M_a(a)) * int(O_aj(a,j)) for j in J for a in A) for k in K for i in I), name = "R9") 
+
+#RESTRICCION TRAYECTO ARREGLAR
+#modelo.addConstrs((float(D_ik(i, k) - (minimo(k))) >= 1-(phi_bki[b,k,i]) for b in B for k in K for i in I), name = "R10")
